1f18189738e24c13ed1e74aaa76c21fb64c3756d
Serghei Marjin <serghei.marjin@gmail.com>
2017-09-26 12:13:00 +0300

    Escape single quotes

--- lib/crowdin/translations/endpoints/update_file_endpoint.rb
+++ lib/crowdin/translations/endpoints/update_file_endpoint.rb
@@ -2,8 +2,8 @@ module Crowdin
   module Translations
     module Endpoints
       class UpdateFileEndpoint < Endpoint
-        def call(files:, update_option: :update_without_changes)
-          RestClient.post(url, files: files, update_option: update_option)
+        def call(files:, update_option: :update_without_changes, escape_quotes: 0)
+          RestClient.post(url, files: files, update_option: update_option, escape_quotes: escape_quotes)
         rescue RestClient::ResourceNotFound, RestClient::BadRequest => error
           Crowdin::Translations::Logger.warning error_message(error), color: :yellow
         end
