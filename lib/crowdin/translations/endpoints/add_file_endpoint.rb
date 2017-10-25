c044737d8bce43c490dae5f99b1ab19bfc4b6711
Serghei Marjin <Serghei.Marjin@4finance.com>
2017-10-25 17:29:39 +0300

    Refactor AddFileEndpoint and UpdateFileEndpoint

--- lib/crowdin/translations/endpoints/add_file_endpoint.rb
+++ lib/crowdin/translations/endpoints/add_file_endpoint.rb
@@ -2,9 +2,9 @@ module Crowdin
   module Translations
     module Endpoints
       class AddFileEndpoint < Endpoint
-        def call(args)
-          RestClient.post(url, files: args)
-        rescue RestClient::BadRequest => error
+        def call(files:)
+          RestClient.post(url, files: files)
+        rescue RestClient::BadRequest, RestClient::InternalServerError => error
           Crowdin::Translations::Logger.warning error_message(error), color: :yellow
         end
       end
--- lib/crowdin/translations/endpoints/update_file_endpoint.rb
+++ lib/crowdin/translations/endpoints/update_file_endpoint.rb
@@ -3,11 +3,7 @@ module Crowdin
     module Endpoints
       class UpdateFileEndpoint < Endpoint
         def call(files:, update_option: :update_without_changes, escape_quotes: 2)
-          files_to_update = files.each_with_object({}) do |pair, updated|
-            pair.each_key { |name| updated[name] = pair[name] }
-          end
-
-          RestClient.post(url, files: files_to_update, update_option: update_option)
+          RestClient.post(url, files: files, update_option: update_option)
         rescue RestClient::ResourceNotFound, RestClient::BadRequest, RestClient::InternalServerError => error
           Crowdin::Translations::Logger.warning error_message(error), color: :yellow
         end
